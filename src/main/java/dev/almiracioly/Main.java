9ed778b6fbf006dc23dde119c4c1f509dc4fcf33
almiracioly <almir.acioly@gmail.com>
2024-02-12 23:39:40 -0300

    First commit!

---
+++ src/main/java/dev/almiracioly/Main.java
@@ -0,0 +1,7 @@
+package dev.almiracioly;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("To the worm who ate away at the cold flesh of my corpse I have dedicated as a loving remembrance these posthumous memoirs");
+    }
+}
